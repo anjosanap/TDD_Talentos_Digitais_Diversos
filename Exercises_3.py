096f56332b6688882527460d1028e6ca38e49408
Ana Paula Anjos <53908857+anjosanap@users.noreply.github.com>
2019-11-12 18:48:44 -0300

    Update Exercises_3.py

--- Exercises_3.py
+++ Exercises_3.py
@@ -1,13 +1,16 @@
-nota1 = float(input("Nota1: "))
+nota1 = float(input("Nota 1: "))
 
-nota2 = float(input('Nota 2: '))
+nota2 = float(input("Nota 2: "))
 
-nota3 = float(input('Nota 3: '))
+nota3 = float(input("Nota 3: "))
 
-nota4 = float(input('Nota 4: '))
+nota4 = float(input("Nota 4: "))
 
 media = (float(nota1) + float(nota2) + float(nota3) + float(nota4))/4
-print (media)
+
+print(" ")
+print ("Sua média foi", media)
+print(" ")
 
 if media > 7:
    print("Você está aprovado")
